6965c98dd087d2cfff5540b00ff0404e6c3d1e8d
AbdulAhad87522 <ahadbahi263@gmail.com>
2025-06-17 21:09:25 +0500

    .

--- PoultryProject/PoultryProject/Program.cs
+++ PoultryProject/PoultryProject/Program.cs
@@ -21,7 +21,7 @@ namespace PoultryProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new chicksform());
+            Application.Run(new Form1());
         }
     }
 }
